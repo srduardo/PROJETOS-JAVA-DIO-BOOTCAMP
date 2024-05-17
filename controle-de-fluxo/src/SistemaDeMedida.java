ff09b3ca268feb80175e88996a89bcb630154ebb
srduardo <eduardodev8@gmail.com>
2024-05-17 19:38:36 -0300

    Condicionais

---
+++ controle-de-fluxo/src/SistemaDeMedida.java
@@ -0,0 +1,28 @@
+public class SistemaDeMedida {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        switch (sigla) {
+            
+            case "P": {
+                System.out.println("Pequeno");
+                break;
+            }
+
+            case "M": {
+                System.out.println("Medio");
+                break;
+            }
+            
+            case "G": {
+                System.out.println("Grande");
+                break;
+            }
+            
+            default: {
+                System.out.println("INDEFINIDO");
+                break;
+            }
+        }
+    }
+}
